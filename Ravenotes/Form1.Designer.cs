7c616a3b5e8656c4962b6b70ccb71dfe4491df2e
Sibdroid <ryabovglebrg@gmail.com>
2023-09-24 16:07:40 +0200

    Add project files.

---
+++ Ravenotes/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ravenotes
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Input = new System.Windows.Forms.RichTextBox();
+			this.FontSize = new System.Windows.Forms.TextBox();
+			this.IncreaseFontSize = new System.Windows.Forms.Button();
+			this.DecreaseFontSize = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// Input
+			// 
+			this.Input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.Input.Font = new System.Drawing.Font("Roboto", 16F);
+			this.Input.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.Input.Location = new System.Drawing.Point(2, 2);
+			this.Input.Name = "Input";
+			this.Input.Size = new System.Drawing.Size(380, 325);
+			this.Input.TabIndex = 0;
+			this.Input.Text = "";
+			// 
+			// FontSize
+			// 
+			this.FontSize.Font = new System.Drawing.Font("Roboto", 20F);
+			this.FontSize.Location = new System.Drawing.Point(2, 333);
+			this.FontSize.Name = "FontSize";
+			this.FontSize.Size = new System.Drawing.Size(80, 40);
+			this.FontSize.TabIndex = 1;
+			this.FontSize.Text = "16";
+			this.FontSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// IncreaseFontSize
+			// 
+			this.IncreaseFontSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.IncreaseFontSize.Font = new System.Drawing.Font("Roboto", 14F);
+			this.IncreaseFontSize.Location = new System.Drawing.Point(2, 379);
+			this.IncreaseFontSize.Name = "IncreaseFontSize";
+			this.IncreaseFontSize.Size = new System.Drawing.Size(37, 37);
+			this.IncreaseFontSize.TabIndex = 2;
+			this.IncreaseFontSize.Text = "▲";
+			this.IncreaseFontSize.UseVisualStyleBackColor = true;
+			this.IncreaseFontSize.Click += new System.EventHandler(this.IncreaseFontSize_Click);
+			// 
+			// DecreaseFontSize
+			// 
+			this.DecreaseFontSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.DecreaseFontSize.Font = new System.Drawing.Font("Roboto", 14F);
+			this.DecreaseFontSize.Location = new System.Drawing.Point(45, 379);
+			this.DecreaseFontSize.Name = "DecreaseFontSize";
+			this.DecreaseFontSize.Size = new System.Drawing.Size(37, 37);
+			this.DecreaseFontSize.TabIndex = 3;
+			this.DecreaseFontSize.Text = "▼";
+			this.DecreaseFontSize.UseVisualStyleBackColor = true;
+			this.DecreaseFontSize.Click += new System.EventHandler(this.DecreaseFontSize_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(384, 421);
+			this.Controls.Add(this.DecreaseFontSize);
+			this.Controls.Add(this.IncreaseFontSize);
+			this.Controls.Add(this.FontSize);
+			this.Controls.Add(this.Input);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox Input;
+		private System.Windows.Forms.TextBox FontSize;
+		private System.Windows.Forms.Button IncreaseFontSize;
+		private System.Windows.Forms.Button DecreaseFontSize;
+	}
+}
+
---
+++ Ravenotes/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ravenotes
+{
+	public partial class Form1 : Form
+	{
+		int defaultFontSize = 16;
+		public Form1()
+		{
+			InitializeComponent();
+			Input.Font = new Font(Input.Font.FontFamily, defaultFontSize);
+			FontSize.Font = new Font(Input.Font.FontFamily, defaultFontSize);
+		}
+
+		private void IncreaseFontSize_Click(object sender, EventArgs e)
+		{
+			defaultFontSize++;
+			Input.Font = new Font(Input.Font.FontFamily, defaultFontSize);
+			FontSize.Text = defaultFontSize.ToString();
+			this.ActiveControl = this.Input;
+		}
+
+		private void DecreaseFontSize_Click(object sender, EventArgs e)
+		{
+			defaultFontSize--;
+			Input.Font = new Font(Input.Font.FontFamily, defaultFontSize);
+			FontSize.Text = defaultFontSize.ToString();
+			this.ActiveControl = this.Input;
+		}
+	}
+}
